ab2e884b4deae4e23f4ebdf9159a64daab280c24
LocNguyen <locn1504@gmail.com>
2024-07-02 16:34:13 +0700

    fix typescript complaints, add toastify package

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Booklist from "./Booklist";
 import { IBook } from "./book.model";
 import Loading from "./core/Loading";
 import Refresh from "./core/Refresh";
 import Title from "./core/Title";
 import useFetch from "./hooks/useFetch";
-
+import { ToastContainer } from "react-toastify";
 const url = "https://potterhead-api.vercel.app/api/books";
 function App() {
 	const [isShowAll, setIsShowAll] = useState(true);
 	const [isLoading, data, fetch, remove] = useFetch<IBook[]>(url);
 
 	if (isLoading) return <Loading />;
+	if (!data) return <h2>No data found!</h2>;
 	if (data.length === 0) return <Refresh refresh={fetch} />;
 	return (
 		<>
+			<ToastContainer position="top-center" />
+
 			<Title title="harry potter books api" />
 			<div className="btn-container">
 				<button
--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
-
-const useFetch = <T>(
+import { toast } from "react-toastify";
+// Define the type for the items in the data array
+interface ItemType {
+	serial: string;
+}
+const useFetch = <T extends ItemType[]>(
 	url: string
 ): [boolean, T | undefined, () => void, (id: string) => void] => {
 	const [data, setData] = useState<T>();
@@ -13,20 +17,20 @@ const useFetch = <T>(
 		setIsLoading(true);
 		try {
 			const response = await fetch(url);
-			const books = await response.json();
-			setData(books);
+			const data = await response.json();
+			setData(data);
 		} catch (error) {
 			console.log(error);
 		}
 		setIsLoading(false);
+		toast.success("Fetching completed. 😋");
 	}
 	//bug khi xoá phần tử cuối đầu tiên lúc đang hiển thị one by one
 	function remove(id: string) {
+		if (!data) return toast.error("Something when wrong! 🤯");
 		const newList = data.filter((item) => item.serial !== id);
-		setData(newList);
-		return;
+		setData(newList as T);
 	}
-
 	return [isLoading, data, fetchData, remove];
 };
 export default useFetch;
